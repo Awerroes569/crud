3425726f0c90c469aede5ab151ab98cd5b950f18
AS <arssoo569@gmail.com>
2024-05-03 13:18:01 +0200

    navbar raw

--- src/components/views/NavBar.js
+++ src/components/views/NavBar.js
@@ -1,7 +1,18 @@
+import styles from './NavBar.module.scss';
+import { Nav, Navbar}  from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
 const NavBar = () => {
     return (
         
-        <h3>NavBar</h3>
+        <Navbar bg="primary" className="justify-content-between">
+            <Navbar.Brand className="text-light">Blog.app</Navbar.Brand>
+            
+            <Nav>
+                <Nav.Link as={NavLink}  to="/">Home</Nav.Link>
+                <Nav.Link as={NavLink} className="text-light" to="/about">About</Nav.Link>
+            </Nav>
+        </Navbar>
         
     );
 }
